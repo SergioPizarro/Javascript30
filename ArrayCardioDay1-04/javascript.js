7e3e09a1ec67db526909e0127a1020ee9654edf9
Sergio Pizarro <sergio.pizarroseg@gmail.com>
2022-06-27 21:37:14 +0200

    first 6 finished

--- ArrayCardioDay1-04/javascript.js
+++ ArrayCardioDay1-04/javascript.js
@@ -23,17 +23,32 @@ const people = [
 
 // Array.prototype.filter()
 // 1. Filter the list of inventors for those who were born in the 1500's
+const fifteen = inventors.filter(inventor => inventor.year < 1600 && inventor.year >= 1500);
+  console.table(fifteen);
 
 // Array.prototype.map()
 // 2. Give us an array of the inventors first and last names
+const fullNames = inventors.map(inventor => inventor.first + " " + inventor.last);
+  console.table(fullNames);
 
 // Array.prototype.sort()
 // 3. Sort the inventors by birthdate, oldest to youngest
+const sortByYear = inventors.sort( (a,b) => a.year > b.year ? 1 : -1);
+console.table(sortByYear);
 
 // Array.prototype.reduce()
 // 4. How many years did all the inventors live all together?
+const totalYears= inventors.reduce((total, currentValue) => total + currentValue.passed - currentValue.year, 0);
+console.log("HOLA");
+console.log(totalYears);
 
 // 5. Sort the inventors by years lived
+const oldest = inventors.sort(function(a, b){
+  const lastGuy = a.passed - a.year;
+  const nextGuy = b.passed - b.year;
+  return lastGuy > nextGuy ? -1 : 1;
+});
+console.table(oldest);
 
 // 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
 // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
